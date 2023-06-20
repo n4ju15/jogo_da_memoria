cc51f904684b7072e3494f38f819b4c0dd027933
Ana Julia Gari  Albano <112116683+n4ju15@users.noreply.github.com>
2023-06-20 16:51:24 -0300

    Scripts

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const cards = [1, 1, 2, 2, 3, 3, 4, 4];
+
+async function generateImagePairs() {
+    const imagePairs = {};
+    for (let i = 0; i < cards.length; i++) {
+        if (!imagePairs[cards[i]]) {
+            const id = Math.floor()
+        }
+    }
+}
